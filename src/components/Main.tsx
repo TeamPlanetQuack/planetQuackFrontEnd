32600fe15b2e91e9115303e3e612cbf9f895f5b7
Alexa Mullin <alemulli@gmail.com>
2023-03-28 14:26:17 -0400

    converting to typescript

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React 
+// { useState, useEffect } 
+from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import {
   PlanetsNavigationBar, 
   SolarSystemHeader,
-} from './'
+} from '.'
 
 const Main = () => {
     return (
---
+++ src/components/SolarSystemHeader.tsx
@@ -0,0 +1,13 @@
+import React
+//  { useState, useEffect } 
+ from "react";
+
+const SolarSystemHeader: React.FC = () => {
+    return (
+      <div>
+        <div className="sunImg"></div>
+      </div>
+    );
+  };
+  
+  export default SolarSystemHeader;

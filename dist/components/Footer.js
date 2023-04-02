67c683f1c09278bf3c5128cb87badca706fc3bbd
Alexa Mullin <alemulli@gmail.com>
2023-04-02 09:51:49 -0400

    links to githubs and linkedins in footer

--- dist/components/Footer.js
+++ dist/components/Footer.js
@@ -1,16 +1,39 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 const Footer = () => {
     return (React.createElement("div", { id: "footer" },
         React.createElement("p", null,
             "All information provided on this site was sourced from",
             " ",
             React.createElement("a", { href: "https://solarsystem.nasa.gov/", target: "_blank" }, "https://solarsystem.nasa.gov/")),
+        React.createElement("p", null,
+            React.createElement(NavLink, { to: "/" }, "Home"),
+            " | ",
+            React.createElement(NavLink, { to: "/dwarfplanets" }, "Dwarf Planets"),
+            " | ",
+            React.createElement(NavLink, { to: "/quiz" }, "Quiz")),
         React.createElement("p", null, "Made by TeamPlanetQuack"),
         React.createElement("ul", null,
-            React.createElement("li", null, "Nelson Luna"),
-            React.createElement("li", null, "Lex Mullin"),
-            React.createElement("li", null, "Jessy Piesco"),
-            React.createElement("li", null, "Maaya Yamauchi"))));
+            React.createElement("li", null,
+                "Nelson Luna - ",
+                React.createElement("a", { href: "https://github.com/xKurieita", target: "_blank" }, "Github"),
+                " - ",
+                React.createElement("a", { href: "https://www.linkedin.com/in/nelson-luna/", target: "_blank" }, "LinkedIn")),
+            React.createElement("li", null,
+                "Lex Mullin - ",
+                React.createElement("a", { href: "https://github.com/alemulli", target: "_blank" }, "Github"),
+                " - ",
+                React.createElement("a", { href: "https://www.linkedin.com/in/alexa-mullin/", target: "_blank" }, "LinkedIn")),
+            React.createElement("li", null,
+                "Jessy Piesco - ",
+                React.createElement("a", { href: "https://github.com/JessyPiesco", target: "_blank" }, "Github"),
+                " - ",
+                React.createElement("a", { href: "https://www.linkedin.com/in/jessypiesco/", target: "_blank" }, "LinkedIn")),
+            React.createElement("li", null,
+                "Maaya Yamauchi - ",
+                React.createElement("a", { href: "https://github.com/mayamauchi", target: "_blank" }, "Github"),
+                " - ",
+                React.createElement("a", { href: "https://www.linkedin.com/in/maaya-yamauchi/", target: "_blank" }, "LinkedIn")))));
 };
 export default Footer;
 //# sourceMappingURL=Footer.js.map
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom"
 
 const Footer: React.FC = () => {
   return (
@@ -9,12 +10,13 @@ const Footer: React.FC = () => {
           https://solarsystem.nasa.gov/
         </a>
       </p>
+      <p><NavLink to="/">Home</NavLink> | <NavLink to="/dwarfplanets">Dwarf Planets</NavLink> | <NavLink to="/quiz">Quiz</NavLink></p>
       <p>Made by TeamPlanetQuack</p>
       <ul>
-        <li>Nelson Luna</li>
-        <li>Lex Mullin</li>
-        <li>Jessy Piesco</li>
-        <li>Maaya Yamauchi</li>
+        <li>Nelson Luna - <a href="https://github.com/xKurieita" target="_blank">Github</a> - <a href="https://www.linkedin.com/in/nelson-luna/" target="_blank">LinkedIn</a></li>
+        <li>Lex Mullin - <a href="https://github.com/alemulli" target="_blank">Github</a> - <a href="https://www.linkedin.com/in/alexa-mullin/" target="_blank">LinkedIn</a></li>
+        <li>Jessy Piesco - <a href="https://github.com/JessyPiesco" target="_blank">Github</a> - <a href="https://www.linkedin.com/in/jessypiesco/" target="_blank">LinkedIn</a></li>
+        <li>Maaya Yamauchi - <a href="https://github.com/mayamauchi" target="_blank">Github</a> - <a href="https://www.linkedin.com/in/maaya-yamauchi/" target="_blank">LinkedIn</a></li>
       </ul>
     </div>
   );

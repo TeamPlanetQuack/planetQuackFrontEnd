363c4f9578c1787b3863752b83146b96f2b9db36
Alexa Mullin <alemulli@gmail.com>
2023-04-02 09:52:50 -0400

    links to githubs and linkedins in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom"
 const Footer: React.FC = () => {
   return (
     <div id="footer">
+      <p><NavLink to="/">Home</NavLink> | <NavLink to="/dwarfplanets">Dwarf Planets</NavLink> | <NavLink to="/quiz">Quiz</NavLink></p>
       <p>
         All information provided on this site was sourced from{" "}
         <a href="https://solarsystem.nasa.gov/" target="_blank">
           https://solarsystem.nasa.gov/
         </a>
       </p>
-      <p><NavLink to="/">Home</NavLink> | <NavLink to="/dwarfplanets">Dwarf Planets</NavLink> | <NavLink to="/quiz">Quiz</NavLink></p>
       <p>Made by TeamPlanetQuack</p>
       <ul>
         <li>Nelson Luna - <a href="https://github.com/xKurieita" target="_blank">Github</a> - <a href="https://www.linkedin.com/in/nelson-luna/" target="_blank">LinkedIn</a></li>
